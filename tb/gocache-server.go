2be4edaece839f6a1d8ca04a41b9798db5d5f7f7
Brad Fitzpatrick <bradfitz@tailscale.com>
2023-10-27 20:17:15 -0700

    tb: add some metrics to gocache service
    Signed-off-by: Brad Fitzpatrick <bradfitz@tailscale.com>

--- tb/gocache-server.go
+++ tb/gocache-server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"expvar"
 	"io"
 	"log"
 	"net/http"
@@ -10,7 +11,22 @@ import (
 	"time"
 
 	"github.com/bradfitz/go-tool-cache/cachers"
+	"tailscale.com/metrics"
 )
+
+var (
+	metricPut                 = expvar.NewInt("gocache_put")
+	metricActionGetHit        = expvar.NewInt("gocache_action_get_hit")
+	metricActionGetMiss       = expvar.NewInt("gocache_action_get_miss")
+	metricActionOuputHit      = expvar.NewInt("gocache_output_get_hit")
+	metricActionOuputHitBytes = expvar.NewInt("gocache_output_get_hit_bytes")
+	metricActionOutputMiss    = expvar.NewInt("gocache_output_get_miss")
+	metricGoCacheErr          = &metrics.LabelMap{Label: "err"}
+)
+
+func init() {
+	expvar.Publish("gocache_error", metricGoCacheErr)
+}
 
 type goCacheServer struct {
 	cache   *cachers.DiskCache // TODO: add interface for things other than disk cache? when needed.
@@ -68,6 +84,7 @@ func validHex(x string) bool {
 func (s *goCacheServer) handleGetAction(w http.ResponseWriter, r *http.Request) {
 	actionID, ok := getHexSuffix(r, "/action/")
 	if !ok {
+		metricGoCacheErr.Add("action_suffix", 1)
 		http.Error(w, "bad request", http.StatusBadRequest)
 		return
 	}
@@ -75,22 +92,27 @@ func (s *goCacheServer) handleGetAction(w http.ResponseWriter, r *http.Request) 
 	ctx := r.Context()
 	outputID, diskPath, err := s.cache.Get(ctx, actionID)
 	if err != nil {
+		metricGoCacheErr.Add("action_get", 1)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	if outputID == "" {
+		metricGoCacheErr.Add("action_empty_outputid", 1)
 		http.Error(w, "not found ()", http.StatusNotFound)
 		return
 	}
 	fi, err := os.Stat(diskPath)
 	if err != nil {
 		if os.IsNotExist(err) {
+			metricActionGetMiss.Add(1)
 			http.Error(w, "not found (post-stat)", http.StatusNotFound)
 			return
 		}
+		metricGoCacheErr.Add("action_state", 1)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+	metricActionGetHit.Add(1)
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(&cachers.ActionValue{
 		OutputID: outputID,
@@ -104,8 +126,21 @@ func (s *goCacheServer) handleGetOutput(w http.ResponseWriter, r *http.Request) 
 		http.Error(w, "bad request", http.StatusBadRequest)
 		return
 	}
+	filename := s.cache.OutputFilename(outputID)
+	if fi, err := os.Stat(filename); err != nil {
+		if os.IsNotExist(err) {
+			metricActionOutputMiss.Add(1)
+		} else {
+			metricGoCacheErr.Add("output_stat_err", 1)
+		}
+	} else if fi.Mode().IsRegular() {
+		metricActionOuputHit.Add(1)
+		metricActionOuputHitBytes.Add(fi.Size())
+	} else {
+		metricGoCacheErr.Add("output_stat_nonreg", 1)
+	}
 	w.Header().Set("Content-Type", "application/octet-stream")
-	http.ServeFile(w, r, s.cache.OutputFilename(outputID))
+	http.ServeFile(w, r, filename)
 }
 
 func (s *goCacheServer) handlePut(w http.ResponseWriter, r *http.Request) {
@@ -116,17 +151,21 @@ func (s *goCacheServer) handlePut(w http.ResponseWriter, r *http.Request) {
 	}
 	actionID, outputID, ok := strings.Cut(r.RequestURI[len("/"):], "/")
 	if !ok || !validHex(actionID) || !validHex(outputID) {
+		metricGoCacheErr.Add("put_bad_uri", 1)
 		http.Error(w, "bad URI", http.StatusBadRequest)
 		return
 	}
 	if r.ContentLength == -1 {
+		metricGoCacheErr.Add("put_content_len", 1)
 		http.Error(w, "missing Content-Length", http.StatusBadRequest)
 		return
 	}
 	_, err := s.cache.Put(ctx, actionID, outputID, r.ContentLength, r.Body)
 	if err != nil {
+		metricGoCacheErr.Add("put_err", 1)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+	metricPut.Add(1)
 	w.WriteHeader(http.StatusNoContent)
 }
